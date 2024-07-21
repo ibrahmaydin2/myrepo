057a003251fbf4978019a37e57fabdb51358acaf
ibrahimaydin <ibrahmaydin2@gmail.com>
2024-07-22 00:19:22 +0300

    added unpacking example

---
+++ unpaching.py
@@ -0,0 +1,7 @@
+#If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+fruits = ["banana","apple","lemon","watermelon"]
+x, y, z, q = fruits
+print(x)
+print(y)
+print(z)
+print(q)
