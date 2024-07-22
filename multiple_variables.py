5fd0c9e9efed57971bfca44167c4b28486b332aa
ibrahimaydin <ibrahmaydin2@gmail.com>
2024-07-22 23:50:36 +0300

    This file describes multiple variables

---
+++ multiple_variables.py
@@ -0,0 +1,15 @@
+#assign values to multiple variables in one line
+x, y, z= "orange", "apple", "banana"
+print(x,y,z)
+#or
+print(x)
+print(y)
+print(z)
+
+#assign the same value to multiple variables in one line
+x = y = z= "banana"
+print(x,y,z)
+#or
+print(x)
+print(y)
+print(z)
