35c133060d676c649a8cf19ed19fb277f57d1a17
ibrahimaydin <ibrahmaydin2@gmail.com>
2024-07-22 23:57:10 +0300

    this file describes output variables

---
+++ output_variables.py
@@ -0,0 +1,21 @@
+#The Python print() function is often used to output variables.
+x = "Python testing"
+print(x)
+#In the print() function, you output multiple variables, separated by a comma
+x = "This"
+y = "file"
+z = "describes"
+q = "output"
+w = "variables"
+print(x,y,z,q,w)
+#You can also use the + operator to output multiple variables:
+x = "This "
+y = "file "
+z = "describes "
+q = "output "
+w = "variables "
+print(x+y+z+q+w)
+#For numbers, the + character works as a mathematical operator:
+x = 5
+y = 10
+print(x+y)
